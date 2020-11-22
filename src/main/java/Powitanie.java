02330a39f17ca89457f09c4fafe7ee22f59ef69f
kacper022 <kcpr543@gmail.com>
2020-11-22 11:48:53 +0100

    pierwszy commit

---
+++ src/main/java/Powitanie.java
@@ -0,0 +1,12 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.PrintWriter;
+
+@WebServlet("/index")
+public class Powitanie extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request){
+
+    }
+}
